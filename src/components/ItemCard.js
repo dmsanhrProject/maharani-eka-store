a5a32b9831a240a4173b99690220357d03138643
Dimas Atmodjo <dimasputra162003@gmail.com>
2024-08-28 09:51:16 +0700

    -u shop page discount

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card} from 'react-bootstrap';
+import { Card, Badge} from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
 const ItemCard = ({ item, brandName }) => {
@@ -14,14 +14,21 @@ const ItemCard = ({ item, brandName }) => {
   };
 
   return (
-    <Card className='border-0 shadow-sm rounded-0' onClick={handleClick} style={{cursor:"pointer"}}>
+    <Card className='h-100 border-0 shadow-sm rounded-0' onClick={handleClick} style={{cursor:"pointer"}}>
       <Card.Img variant="top" className='rounded-0' src={item.images[0]} />
       <h6 className='text-center bg-red mb-0 py-1 px-3 text-white text-truncate text-uppercase' style={{borderTopLeftRadius:'100%',borderTopRightRadius:'100%',marginTop:'-26px'}}>{brandName}</h6>
       <Card.Body>
         <Card.Text className='text-truncate text-muted card-title-name'>{item.name}</Card.Text>
         <Card.Title className='card-title-price'>
-          Rp{formatRupiah( parseInt(item.price))}
+          Rp{item.discount === 0 || item.discount === '0' ? formatRupiah( parseInt(item.price)) : formatRupiah( parseInt(item.discount))}
         </Card.Title>
+        <Card.Text style={{fontSize:'12px'}} className={item.discount === 0 || item.discount === '0' ? 'd-none' : 'd-block'}>
+          {/* Rp{formatRupiah( parseInt(item.discount))} */}
+          
+          <span className='text-strip-discount text-grey'>Rp{formatRupiah( parseInt(item.price))}</span> 
+          <Badge bg='discount' className='ms-1 p-1'>{(((parseInt(item.price) - parseInt(item.discount)) / parseInt(item.price)) * 100).toFixed(0)}%</Badge>
+        
+        </Card.Text>
       </Card.Body>
     </Card>
   );
